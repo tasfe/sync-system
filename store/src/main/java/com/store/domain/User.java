185850e8512408a875cbd4dfe337588ebf5ab7f0
hover.sasa@gmail.com <hover.sasa@gmail.com@951898de-6b84-cd7c-c021-6082511f1c2a>
2013-02-01 01:26:52 +0000

    用户实体更新

--- store/src/main/java/com/store/domain/User.java
+++ store/src/main/java/com/store/domain/User.java
@@ -1,16 +1,20 @@
 package com.store.domain;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
-public class User {
+public class User implements BaseEntity{
+	@Override
+	public String tableName() {
+		return "syn_account";
+	}
 
-	private long id;
-	
+	private long id;	
 	//登录账号
 	private String loginName;
-	private String password;
-	
+	private String password;	
 	//显示名
 	private String userName;
 	
@@ -20,10 +24,11 @@ public class User {
 	//使用数字代表权限：1:超级管理员 2：管理员  3：兼职  .....
 	private Integer purview;
 	
-	//是否禁用
-	private boolean disable;
 
-	public long getId() {
+	private int status;
+
+	@SuppressWarnings("unchecked")
+	public Long getId() {
 		return id;
 	}
 
@@ -62,13 +67,13 @@ public class User {
 	public void setShops(Set<Shop> shops) {
 		this.shops = shops;
 	}
-
-	public boolean isDisable() {
-		return disable;
+	
+	public int getStatus() {
+		return status;
 	}
 
-	public void setDisable(boolean disable) {
-		this.disable = disable;
+	public void setStatus(int status) {
+		this.status = status;
 	}
 
 	public Integer getPurview() {
@@ -79,4 +84,15 @@ public class User {
 		this.purview = purview;
 	}
 
+	@Override
+	public Map<String, Object[]> getFieldInfo() {		
+		Map<String, Object[]> map = new HashMap<String, Object[]>();
+		map.put("id", new Object[] { id, "setId", long.class });
+		map.put("account", new Object[] { loginName, "setLoginName", String.class });
+		map.put("password", new Object[]{password, "setPassword", int.class});
+		map.put("username", new Object[]{userName, "setUserName", String.class});
+		map.put("purview", new Object[] {purview, "setPurview", int.class });
+		map.put("status", new Object[] {status, "setStatus", int.class });		
+		return map;
+	}
 }
